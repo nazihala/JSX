f39e17c6903bfaf36b48378b66369dc8508ebcf4
nazihala <naziha.lahrach@gmail.com>
2020-07-14 23:37:04 +0200

    Updates

---
+++ precache-manifest.1e6cf62c3a735237665efe93bdb4faeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b6aa35bc3f4dde9daac47207b8100b",
+    "url": "/JSX/index.html"
+  },
+  {
+    "revision": "f67d0d49bdee4ba80736",
+    "url": "/JSX/static/css/main.dc40e617.chunk.css"
+  },
+  {
+    "revision": "c8c85fe7542de8dde514",
+    "url": "/JSX/static/js/2.9ffd4e73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JSX/static/js/2.9ffd4e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67d0d49bdee4ba80736",
+    "url": "/JSX/static/js/main.da7c621d.chunk.js"
+  },
+  {
+    "revision": "0b1f935f1ca280c70ccc",
+    "url": "/JSX/static/js/runtime-main.264cc32d.js"
+  },
+  {
+    "revision": "2214e38ad5985a042aba592db2cb3891",
+    "url": "/JSX/static/media/imageInsrc.2214e38a.jpg"
+  }
+]);
